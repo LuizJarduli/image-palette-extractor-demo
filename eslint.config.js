e47b82e9aa2f14235bb485d6f5882cf392d7d2e4
Luiz Jarduli <luizjarduli@gmail.com>
2024-12-29 19:49:54 -0300

    feat: initializing the Demo widget for the input, in progress

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
-import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
   ...eslintPluginAstro.configs.recommended,
-  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
+  ...eslintPluginAstro.configs['react-recommended'],
+  ...eslintPluginAstro.configs['react-hooks-recommended'],
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.astro"],
+    files: ['**/*.ts', '**/*.tsx', '**/*.astro'],
     rules: {
-      "astro/no-exports-from-components": "error",
+      'astro/no-exports-from-components': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/prop-types': 'off',
     },
   },
 ];
---
+++ src/components/DemoWidget.tsx
@@ -0,0 +1,8 @@
+
+export default function DemoWidget() {
+  return (
+    <div>
+      <h1>In Progress</h1>
+    </div>
+  );
+}
