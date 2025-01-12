b6e692734b782a5e2f8af1f2d738980a96e5808f
Luiz Jarduli <luizjarduli@gmail.com>
2025-01-12 20:14:36 -0300

    feat: developing the upload widget, still in progress

--- src/components/react/UploadWidget.tsx
+++ src/components/react/UploadWidget.tsx
@@ -8,17 +8,30 @@ const DropzoneContainer = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 200px;
   margin: 0 auto;
   border-radius: 10px;
   background-color: #22222a;
   cursor: pointer;
+  row-gap: 0.5rem;
+  padding: 2rem;
+`;
+
+const UploadIcon = styled.img`
+  display: block;
+  width: 3.125rem;
+`;
+
+const CustomText = styled.p`
+  font-size: 0.75rem;
+  color: #fff;
 `;
 
 export default function UploadWidget(): JSX.Element {
   return (
     <DropzoneContainer>
-      <img src={uploadIcon} alt="Upload Icon" />
+      <UploadIcon src={uploadIcon} alt="Upload Icon" />
+      <CustomText>Drop images here</CustomText>
+      <CustomText>Or</CustomText>
     </DropzoneContainer>
   );
 }
