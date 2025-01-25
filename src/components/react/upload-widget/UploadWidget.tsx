35428f022fdf4e46a05aa7fd5e9ef2830d4f6883
Luiz Jarduli <luizjarduli@gmail.com>
2025-01-25 19:41:34 -0300

    feat: upload progress, now the drag and the upload button extract up to ten colors, the UX is being made yet

--- src/components/react/upload-widget/UploadWidget.tsx
+++ src/components/react/upload-widget/UploadWidget.tsx
@@ -4,23 +4,79 @@ import { Button } from '../button/Button';
 import { CustomText, DropzoneContainer, HiddenInputFile, UploadIcon } from './styles';
 import { useRef, useState } from 'react';
 import { useReleaseUpload } from '../release-upload/ReleaseUploadHook';
+import { AcceptedExtensions } from './AcceptedExtensions';
+import { ColorPalette } from 'image-palette-extractor';
 
 export default function UploadWidget(): JSX.Element {
   const [isDragOver, setDragOver] = useState<boolean>(false);
   const { showAlert, hideAlert } = useReleaseUpload();
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleDragOver = (event: React.DragEvent<HTMLDivElement>, isDraggingOver: boolean) => {
-    event.preventDefault();
+  const getAllowedExtensions = (): string => {
+    return Object.keys(AcceptedExtensions)
+      .map(extension => `image/${extension}`)
+      .join(',');
+  };
 
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>, isDraggingOver: boolean) => {
     const actionHandler = isDraggingOver ? showAlert : hideAlert;
+
+    event.stopPropagation();
+    event.preventDefault();
 
     setDragOver(isDraggingOver);
     actionHandler();
   };
 
-  const handleOnDrop = (event: React.DragEvent<HTMLDivElement>) => {
+  const handleOnDrop = async (event: React.DragEvent<HTMLDivElement>) => {
     handleDragOver(event, false);
+    const { files } = event.dataTransfer;
+    const file = files.item(0);
+
+    console.log(await extractImagePalette(file));
+  };
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+
+      console.log(await extractImagePalette(file));
+    }
+  };
+
+  const extractImagePalette = async (file: File | null): Promise<string[] | null> => {
+    if (!file) return null;
+
+    const base64 = await loadFileBase64(file);
+
+    if (!base64) return null;
+
+    try {
+      return ColorPalette.getColors(base64, 10);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const loadFileBase64 = async (file: File): Promise<string | null> => {
+    const reader = new FileReader();
+
+    return new Promise<string | null>((resolve, reject) => {
+      reader.onerror = () => {
+        reject();
+      };
+
+      reader.onloadend = () => {
+        const base64String = reader.result as string;
+
+        resolve(base64String);
+      };
+
+      reader.readAsDataURL(file);
+    });
   };
 
   return (
@@ -36,7 +92,15 @@ export default function UploadWidget(): JSX.Element {
       <Button label="Upload a file" size="small" onClick={() => inputRef.current?.click()} />
 
       {/* Input file */}
-      <HiddenInputFile ref={inputRef} type="file" id="released-file" aria-hidden="true" />
+      <HiddenInputFile
+        ref={inputRef}
+        type="file"
+        id="released-file"
+        aria-hidden="true"
+        multiple={false}
+        accept={getAllowedExtensions()}
+        onChange={handleFileChange}
+      />
     </DropzoneContainer>
   );
 }
