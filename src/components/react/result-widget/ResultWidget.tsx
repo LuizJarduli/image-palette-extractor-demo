7164978310b29c9d26cb307b89abbafce65207c9
Luiz Jarduli <luizjarduli@gmail.com>
2025-02-06 19:58:12 -0300

    feat(ResultWidget): add footer with upload button and responsive styles

--- src/components/react/result-widget/ResultWidget.tsx
+++ src/components/react/result-widget/ResultWidget.tsx
@@ -1,6 +1,14 @@
 import type { JSX } from 'react';
-import { ColorContainer, Divider, ImageContainer, ResultWidgetContainer } from './style';
+import {
+  ColorContainer,
+  Divider,
+  FooterContainer,
+  ImageContainer,
+  ResultWidgetContainer,
+} from './style';
 import CardColor from '../card-color/CardColor';
+import { Button } from '../button/Button';
+import { useUploadContext } from '../upload-widget/UploadContext';
 
 export interface ResultWidgetProps {
   colors: string[];
@@ -8,17 +16,29 @@ export interface ResultWidgetProps {
 }
 
 export default function ResultWidget({ colors, imageSrc }: ResultWidgetProps): JSX.Element {
+  const { setLoadingState, setExtractedColors } = useUploadContext();
+
+  const resetToUploadState = () => {
+    setLoadingState(null);
+    setExtractedColors(null);
+  };
+
   return (
-    <ResultWidgetContainer>
-      <ImageContainer>
-        <img src={imageSrc} alt="Uploaded image" />
-      </ImageContainer>
-      <Divider />
-      <ColorContainer>
-        {colors.map((color, index) => (
-          <CardColor key={`${color}-${index}`} color={color} />
-        ))}
-      </ColorContainer>
-    </ResultWidgetContainer>
+    <>
+      <ResultWidgetContainer>
+        <ImageContainer>
+          <img src={imageSrc} alt="Uploaded image" />
+        </ImageContainer>
+        <Divider />
+        <ColorContainer>
+          {colors.map((color, index) => (
+            <CardColor key={`${color}-${index}`} color={color} />
+          ))}
+        </ColorContainer>
+      </ResultWidgetContainer>
+      <FooterContainer>
+        <Button label="Upload a new file" size="small" onClick={resetToUploadState} />
+      </FooterContainer>
+    </>
   );
 }
--- src/components/react/result-widget/style.ts
+++ src/components/react/result-widget/style.ts
@@ -8,6 +8,11 @@ export const ResultWidgetContainer = styled.section`
   align-items: center;
   background-color: transparent;
   position: relative;
+
+  @media screen and (max-width: 48rem) {
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 `;
 
 export const Divider = styled.div`
@@ -15,6 +20,12 @@ export const Divider = styled.div`
   background-color: rgb(111, 111, 111);
   margin: 0 1rem;
   align-self: stretch;
+
+  @media screen and (max-width: 48rem) {
+    margin: 1rem 0;
+    width: 100%;
+    height: 1px;
+  }
 `;
 
 export const ColorContainer = styled.div`
@@ -23,6 +34,7 @@ export const ColorContainer = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(8.25rem, 1fr));
   grid-row-gap: 0.5rem;
   grid-column-gap: 0.5rem;
+  align-self: stretch;
 `;
 
 export const ImageContainer = styled.div`
@@ -37,5 +49,17 @@ export const ImageContainer = styled.div`
     max-height: auto;
     border-radius: 0.5rem;
     object-fit: contain;
+  }
+`;
+
+export const FooterContainer = styled.footer`
+  display: flex;
+  width: 100%;
+  height: 5rem;
+  align-items: flex-end;
+  justify-content: flex-end;
+
+  @media screen and (max-width: 48rem) {
+    justify-content: center;
   }
 `;
